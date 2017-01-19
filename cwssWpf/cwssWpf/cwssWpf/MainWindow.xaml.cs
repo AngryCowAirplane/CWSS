f5e3c901c0e023d535c28601e493fdf719d06088
derek <derek@DESKTOP-Q3P0NPB>
2017-01-19 08:34:18 -0600

    test

--- cwssWpf/cwssWpf/cwssWpf/MainWindow.xaml.cs
+++ cwssWpf/cwssWpf/cwssWpf/MainWindow.xaml.cs
@@ -59,6 +59,9 @@ namespace cwssWpf
 
         private void btnCheckIn_Click(object sender, RoutedEventArgs e)
         {
+            // TODO:
+            // validate input in tbUserId
+            // if valid checkIn()
             checkIn();
         }
 
