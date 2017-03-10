d3dcc534cb30c74d7433e75ea9dced5eec104cd4
Derek <Derek@DEREK-PC>
2017-03-10 16:09:23 -0600

    Added explanation of image stuff to settings ideas, and update image test project to make signature black and white

--- cwssWpf/cwssWpf/ImageManipTesting/Program.cs
+++ cwssWpf/cwssWpf/ImageManipTesting/Program.cs
@@ -1,6 +1,8 @@
 ﻿using ImageProcessor;
 using ImageProcessor.Imaging;
+using ImageProcessor.Imaging.Filters.Photo;
 using ImageProcessor.Imaging.Formats;
+using ImageProcessor.Processors;
 using System;
 using System.Collections.Generic;
 using System.Drawing;
@@ -19,7 +21,7 @@ namespace ImageManipTesting
             var file = GetFolderPath(SpecialFolder.DesktopDirectory) + "\\waiver.png";
             byte[] photoBytes = File.ReadAllBytes(file);
 
-            var overlay = GetFolderPath(SpecialFolder.DesktopDirectory) + "\\signature.jpg";
+            var overlay = GetFolderPath(SpecialFolder.DesktopDirectory) + "\\signatures.jpg";
             byte[] overlayBytes = File.ReadAllBytes(overlay);
 
             ISupportedImageFormat format = new PngFormat { Quality = 70 };
@@ -31,6 +33,10 @@ namespace ImageManipTesting
                 {
                     using (ImageFactory imageFactory = new ImageFactory(preserveExifData:true))
                     {
+                        // signature
+                        imageFactory.Load(overlay).Contrast(100).Filter(MatrixFilters.BlackWhite).Save(overlay);
+
+
                         var layer = new ImageLayer();
                         layer.Image = Image.FromFile(overlay);
                         layer.Position = new Point(400, 1225);
